511f4039de8f76a8f0ff506127d5121c3fc3c080
Arab Kumar <arabkumar@Arabs-MacBook-Pro.local>
2023-10-13 16:27:29 +0530

    chore: added futureprovider example

---
+++ lib/examples/future_provider_example.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+enum City { Mysore, Banglore, Bokaro, Kashmir }
+
+final currentCityProvider = StateProvider<City?>((ref) => null);
+
+typedef WeatherEmoji = String;
+
+Future<WeatherEmoji> getWeather(City city) async {
+  return Future.delayed(const Duration(seconds: 1), () {
+    return {
+      City.Mysore: '🍃',
+      City.Banglore: '🌦️',
+      City.Bokaro: '🌞',
+      City.Kashmir: '❄️'
+    }[city]!;
+  });
+}
+
+final FutureProvider<WeatherEmoji> weatherProvider = FutureProvider((ref) {
+  final city = ref.watch(currentCityProvider);
+  if (city != null) {
+    return getWeather(city);
+  } else {
+    return '𖡄';
+  }
+});
+
+class FutureProviderExample extends ConsumerWidget {
+  const FutureProviderExample({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentCityWeather = ref.watch(weatherProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Riverpod Practice"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Container(
+            alignment: Alignment.center,
+            height: 100,
+            child: currentCityWeather.when(
+                data: (data) => Text(
+                      data,
+                      style: const TextStyle(fontSize: 80),
+                    ),
+                error: (error, stackTrace) => Text(stackTrace.toString()),
+                loading: () => const CircularProgressIndicator(strokeWidth: 1)),
+          ),
+          ListView.builder(
+            padding: const EdgeInsets.all(4),
+            itemBuilder: (context, index) {
+              final currentCity = ref.watch(currentCityProvider);
+              bool isSelected = currentCity == City.values[index];
+              return ListTile(
+                title: Text(City.values[index].name),
+                trailing: isSelected ? Icon(Icons.check) : null,
+                onTap: () {
+                  ref.read(currentCityProvider.notifier).state =
+                      City.values[index];
+                },
+              );
+            },
+            itemCount: City.values.length,
+            shrinkWrap: true,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:riverpod_practice/examples/future_provider_example.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -17,71 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
-      home: const MyHomePage(),
-    );
-  }
-}
-
-enum City { Mysore, Banglore, Bokaro, Kashmir }
-
-typedef WeatherEmoji = String;
-
-Future<WeatherEmoji> getWeather(City? city) async {
-  return {
-        'Mysore': '🍃',
-        'Banglore': '🌦️',
-        'Bokaro': '☀️',
-        'Kashmir': '❄️'
-      }[city] ??
-      '🤷🏼‍♂️';
-}
-
-final StateProvider<City?> currentCityProvider =
-    StateProvider<City?>((ref) => null);
-
-final weatherProvider = FutureProvider<WeatherEmoji>((ref) {
-  final city = ref.watch(currentCityProvider);
-  return getWeather(city);
-});
-
-class MyHomePage extends ConsumerWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Riverpod Practice"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Consumer(
-            builder: (context, ref, child) {
-              final currentWeather = ref.watch(weatherProvider);
-              return currentWeather.when(
-                data: (data) {
-                  return Text(data);
-                },
-                error: (error, stackTrace) => Text("Something went wrong"),
-                loading: () => CircularProgressIndicator(),
-              );
-            },
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: City.values.length,
-            itemBuilder: (context, index) {
-              final City? city = ref.watch(currentCityProvider);
-              bool isSelected = City.values[index] == currentCityProvider;
-              return ListTile(
-                title: Text(city.toString()),
-                leading: isSelected ? Icon(Icons.check) : null,
-              );
-            },
-          )
-        ],
-      ),
+      home: const FutureProviderExample(),
     );
   }
 }
